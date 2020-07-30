bdd647a358eeff8da067d1bd2e97a391564acabb
haphelon <ombewaamos@gmail.com>
2020-07-30 11:53:29 +0300

    Font size Editor added

--- app/src/main/java/com/haphelon/wysiwygeditor/MainActivity.kt
+++ app/src/main/java/com/haphelon/wysiwygeditor/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.haphelon.wysiwygeditor
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import jp.wasabeef.richeditor.RichEditor
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var editor: RichEditor
+    private lateinit var fontSizeEditor: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         editor = findViewById(R.id.editor)
+        editor.setBackgroundColor(R.color.colorEditorBg)
+        fontSizeEditor = findViewById(R.id.fontSizeEditText);
+        fontSizeEditor.addTextChangedListener(textWatcher())
     }
 
     fun centerText(view: View) {
@@ -21,5 +28,21 @@ class MainActivity : AppCompatActivity() {
 
     fun boldText(view: View) {
         editor.setBold()
+    }
+
+    private fun textWatcher(): TextWatcher {
+        return object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val size: String = p0.toString()
+                val x: Int = if (size == "") 0 else size.toInt()
+                editor.setFontSize(x)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        }
     }
 }
